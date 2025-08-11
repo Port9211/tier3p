3ae10d0e7bf8083b44c3df36ca638f687551835d
Port9211 <129165189+Port9211@users.noreply.github.com>
2025-08-11 15:38:00 +0530

    Update variables.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" { default = "us-east-1" }
 variable "cluster_name" { default = "my-eks-cluster" }
-variable "vpc_id" { default = "vpc-xxxxxxxx" }
-variable "private_subnet_ids" { default = ["subnet-aaaa1111", "subnet-bbbb2222"] }
-variable "public_subnet_ids" { default = ["subnet-cccc3333", "subnet-dddd4444"] }
+variable "vpc_id" { default = "vpc-0b00f794cf6685220" }
+variable "private_subnet_ids" { default = ["subnet-0c3638ffbab667700", "subnet-064f6dbf0da7ecdd0"] }
+variable "public_subnet_ids" { default = ["subnet-0fda09de7e33150de", "subnet-04b7952074ad43e4b"] }
 variable "owner" { default = "dev-team" }
